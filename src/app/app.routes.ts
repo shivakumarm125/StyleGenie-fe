62d1355489c03cda1e082cee8f14bfbfa0a55604
shivakumarm125 <shivakumarm.2501@gmail.com>
2025-02-21 23:50:00 +0000

    basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './components/user/user-dashboard/user-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '' , component: UserDashboardComponent},
+    {path: 'admin' , component: AdminDashboardComponent}
+];
+
---
+++ src/app/components/admin/admin.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AdminDashboardComponent
+  ],
+  declarations: [
+  ]
+})
+export class AdminModule {}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
